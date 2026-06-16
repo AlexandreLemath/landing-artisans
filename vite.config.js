f26b2bcf2920a2fcf039eee40624c274499c9f71
AlexandreLemath <mistgun9211@gmail.com>
2026-06-16 20:48:38 +0200

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// base: './' permet de publier facilement sur GitHub Pages,
-// même si ton site est hébergé dans un sous-dossier du type /nom-du-repo/
 export default defineConfig({
-  base: './',
+  base: "/landing-artisans/",
   plugins: [react(), tailwindcss()],
 });
